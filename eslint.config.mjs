87585ba38c113dfbeb689ac3f9f323265cd52039
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-05-29 17:37:06 +0100

    - init project - added prettier - updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,109 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/.next/**', '**/dist/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...compat.extends('next/core-web-vitals'),
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: { project: './tsconfig.json' },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'unused-imports': unusedImports,
+      import: importPlugin,
+      'no-relative-import-paths': noRelativeImportPaths,
+      prettier: prettier,
+    },
+    rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        {
+          prefix: '@',
+          allowSameFolder: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+      'import/no-anonymous-default-export': 'off',
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 0,
+          maxBOF: 0,
+        },
+      ],
+      'react/no-unknown-property': [
+        'error',
+        {
+          ignore: [],
+          requireDataLowercase: true,
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['react'],
+              importNames: ['default'],
+              message: "Please use named imports from 'react' instead (e.g., `import { useState } from 'react'`)",
+            },
+            {
+              group: ['next/router'],
+              message: 'Please use next/navigation instead of next/router',
+            },
+          ],
+        },
+      ],
+
+      'prettier/prettier': 'error',
+    },
+  },
+
+  eslintConfigPrettier,
 ];
-
-export default eslintConfig;
