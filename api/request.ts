0b10ab16e73ca2c31528ecb10d3cdb89b716e6f2
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-08-18 21:58:56 +0100

    - updatetd url request

--- api/request.ts
+++ api/request.ts
@@ -4,8 +4,12 @@ import { requestClient } from './request.client';
 export const request = async <TResult, TBody = undefined>(...params: Parameters<typeof requestClient<TResult, TBody>>) => {
   if (typeof window === 'undefined') {
     const imp = await import('./request.server');
+    const [url, options, errorMatchers] = params;
 
-    return await imp.requestServer<TResult, TBody>(...params);
+    const apiUrl = process.env.INTERNAL_API_URL || 'http://localhost:3030';
+    const fullUrl = `${apiUrl}/api${url}`;
+
+    return await imp.requestServer<TResult, TBody>(fullUrl, options, errorMatchers);
   }
 
   return await requestClient<TResult, TBody>(...params);
