d56b83839c8b18fd4ab7662a65339d13a97e4a5a
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-10-03 23:16:16 +0100

    - added one more document to documents generation - power of attorney vehicle

--- schemas/generateDocuments/documentsSchemas.ts
+++ schemas/generateDocuments/documentsSchemas.ts
@@ -8,6 +8,8 @@ import {
   PropertyPowerOfAttorneyFormData,
 } from '@/schemas/generateDocuments/notary/powerOfAttorneySchema';
 
+import { getVehiclePowerOfAttorneySchema, VehiclePowerOfAttorneyFormData } from './notary/powerOfAttorneyVehicleSchema';
+
 export const DOCUMENT_SCHEMAS = {
   [DOCUMENT_TYPE.PAWER_OF_ATTORNEY_PROPERTY]: {
     schema: getPropertyPowerOfAttorneySchema,
@@ -17,6 +19,14 @@ export const DOCUMENT_SCHEMAS = {
     schema: getReceiveDocumentsPowerOfAttorneySchema,
     type: {} as ReceiveDocumentsPowerOfAttorneyFormData,
   },
+  [DOCUMENT_TYPE.powerAttorneyVehicle]: {
+    schema: getVehiclePowerOfAttorneySchema,
+    type: {} as VehiclePowerOfAttorneyFormData,
+  },
 };
 
-export const DOCUMENTS_SCHEMAS_KEYS = [DOCUMENT_TYPE.PAWER_OF_ATTORNEY_PROPERTY, DOCUMENT_TYPE.powerOfAttorneyDocuments] as const;
+export const DOCUMENTS_SCHEMAS_KEYS = [
+  DOCUMENT_TYPE.PAWER_OF_ATTORNEY_PROPERTY,
+  DOCUMENT_TYPE.powerOfAttorneyDocuments,
+  DOCUMENT_TYPE.powerAttorneyVehicle,
+] as const;
